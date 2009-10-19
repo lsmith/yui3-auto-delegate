786c39ec7162e81ee5ff5534193353a8bc584492
Luke Smith <lsmith@yahoo-inc.com>
2009-10-19 00:07:27 -0700

    Initial drop

---
+++ js/auto-delegate.js
@@ -0,0 +1,20 @@
+YUI.add('auto-delegate', function (Y) {
+
+var ET_ON = Y.EventTarget.prototype.on;
+
+Y.Node.prototype.on = function (type) {
+    var args = Y.Array(arguments,0,true),
+        m    = type.match(/(.+?\|)?(.+)?:(.+)$/),
+        meth = ET_ON;
+
+    if (m) {
+        meth    = this.delegate;
+        args[0] = (m[1] || '') + m[3];
+        args.splice(2,0,m[2]);
+    }
+
+    return meth.apply(this, args);
+
+};
+
+},'0.0.1',{requires:['node']});
